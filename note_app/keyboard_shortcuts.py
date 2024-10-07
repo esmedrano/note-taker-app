d14f745542f97794d6b37df83810eeb2f28dab95
Elijah <97810174+esmedrano@users.noreply.github.com>
2024-10-07 00:16:02 -0500

    Delete note_app/keyboard_shortcuts.py

--- note_app/keyboard_shortcuts.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def get_shortcuts():
-	with open('keyboard_shortcuts.json', 'r') as file:
-			data = json.load(file)
-
-	create_new_node = data.get("create new node")
-	shortcuts = [create_new_node]
-	return shortcuts
